ce18e04aefccebe1f6c02aed03dd7e34d4f8b401
Gomgomkim <dhflwlsjfzz@naver.com>
2022-11-23 17:21:55 +0900

    [Add React-Query] 1. Add React-Query module 2. Add Default Settings of React-Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import './App.css';
 import { RecoilRoot } from "recoil";
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
 import Home from './pages/Home';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home/" element={<Home />}></Route>
-          <Route path="/" element={<Navigate replace to="/home" />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home/" element={<Home />}></Route>
+            <Route path="/" element={<Navigate replace to="/home" />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
